c6e84c6e93b94b988d933099b3b1eaa4ea6d4d18
Jan Lehnardt <jan@apache.org>
2011-08-26 13:11:31 +0200

    shuffle repo commands, this is odd

--- build-source-package.sh
+++ build-source-package.sh
@@ -8,8 +8,8 @@ cd $BUILDDIR
   rm -rf $NAME
   mkdir $NAME
   cd $NAME
-    repo init -u https://github.com/couchbase/php-memcached-manifest --mirror
-    repo init -u https://github.com/couchbase/php-memcached-manifest --depth=1 # don't ask
+    repo init -u https://github.com/couchbase/php-memcached-manifest
+    repo init -u https://github.com/couchbase/php-memcached-manifest --mirror --depth=1 # don't ask
     repo sync
   cd ..
   tar -czf $NAME.tgz $NAME
